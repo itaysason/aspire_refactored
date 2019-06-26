b17ed6f80f0c0a3c1e335c5309f96470de4fb234
itaysason <itaysason@mail.tau.ac.il>
2019-06-26 15:34:01 +0300

    changed radius of normalize_background in preprocess

--- aspire/preprocessor/normalize_background.py
+++ aspire/preprocessor/normalize_background.py
@@ -3,7 +3,8 @@ import aspire.utils.common as common
 
 
 def normalize_background(stack, radius=None):
-    _, n, num_images = stack.shape
+    n = stack.shape[1]
+    radius = n // 2 if radius is None else radius
     circle = ~common.disc(n, radius)
     background_pixels = stack[circle]
     mean = np.mean(background_pixels, 0)
